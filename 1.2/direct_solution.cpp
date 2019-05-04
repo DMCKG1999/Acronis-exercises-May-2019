0897cffbc75a6096d680207d7e7ff6288f76307e
Dima Gushchin <dmckg1999@gmail.com>
2019-05-04 12:22:54 +0300

    Add first functions to first solution to second exercise. Idea: 1. Class MatrixTxT checks that game is over. 2. Count all variants and check table as a matrix

---
+++ 1.2/direct_solution.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Dima on 03.05.2019.
+//
+
+#include "direct_solution.h"
+
+
+void dsol () {
+
+}
---
+++ 1.2/direct_solution.h
@@ -0,0 +1,11 @@
+//
+// Created by Dima on 03.05.2019.
+//
+
+#ifndef INC_1_2_DIRECT_SOLUTION_H
+#define INC_1_2_DIRECT_SOLUTION_H
+
+#endif //INC_1_2_DIRECT_SOLUTION_H
+
+
+
